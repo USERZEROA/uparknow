1f22c34952da2243b47ad317e3d322c293af80d5
mertin <zimut@DESKTOP-U7MLP3A>
2025-01-23 14:50:07 -0700

    WebSocket Adjust

--- AI/main.py
+++ AI/main.py
@@ -7,14 +7,14 @@ async def event_pschanged():
         await asyncio.sleep(random.randint(1, 3))
         event_happened = random.choice([True, False])
         if event_happened:
-            print("Something should be sent.")
+            print("event_happened")
             yield True
         else:
             yield False
 
 
 async def main_loop():
-    WEBSOCKET_SERVER_URL = "wss://84b7-2601-681-4d00-2e30-202f-d912-4854-207b.ngrok-free.app/ws-camera"
+    WEBSOCKET_SERVER_URL = "wss://0cc5-2601-681-4d00-2e30-18e0-653b-7419-d644.ngrok-free.app/ws-camera"
     client = WebSocketClient(WEBSOCKET_SERVER_URL)
 
     await client.connect()
@@ -28,8 +28,15 @@ async def main_loop():
 
         async for event in event_gen:
             if event: 
-                message = "事件触发消息！"
-                await client.send_message(message)
+                messages = [
+                    """{ "role": 1, "parkingLot": 1, "parkingSpacePosition": [1, 1], "availability": 1 }""",
+                    """{ "role": 1, "parkingLot": 2, "parkingSpacePosition": [1, 2], "availability": 1 }""",
+                    """{ "role": 1, "parkingLot": 2, "parkingSpacePosition": [1, 1], "availability": 0 }""",
+                    """{ "role": 1, "parkingLot": 2, "parkingSpacePosition": [1, 1], "availability": 1 }"""
+                ]
+
+                selected_message = random.choice(messages)
+                await client.send_message(selected_message)
 
     except asyncio.CancelledError:
         print("Main loop is end.")
