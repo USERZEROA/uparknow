3ad0c19dcb09374aa1592b17351e9fe414388f31
USERZEROA <112025002+USERZEROA@users.noreply.github.com>
2025-02-28 11:44:22 -0700

    Clean up: remove redundant nested project files

--- uparknow/uparknow/uparknow/src/main/java/edu/utah/cs/uparknow/repository/ParkingLotBoundsRepository.java
+++
@@ -1,8 +0,0 @@
-package edu.utah.cs.uparknow.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import edu.utah.cs.uparknow.model.ParkingLotBounds;
-
-public interface ParkingLotBoundsRepository extends JpaRepository<ParkingLotBounds, Integer> {
-}
